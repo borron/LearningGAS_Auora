ffae47f36f38fca0386a956b0fbc2b7b1a45326e
Borron <ronnie.bornerot@gmail.com>
2023-09-20 12:40:16 +0200

    The Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyrigth Brink Videogame Studio
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyrigth Brink Videogame Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+
